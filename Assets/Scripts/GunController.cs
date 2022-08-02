ff779310b4993610c838f412753fafab37499d1d
Lemintex <57079368+Lemintex@users.noreply.github.com>
2022-08-02 23:35:27 +0100

    Fixed gun behaviour

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public Transform gunHolder;
+    public Gun initialGun;
+    Gun equippedGun;
+
+    private void Start()
+    {
+        if(initialGun != null)
+        {
+           Equip(initialGun);
+        }
+    }
+    public void Equip(Gun gunToEquip)
+    {
+        if(equippedGun != null)
+        {
+            Destroy(equippedGun.gameObject);
+        }
+        equippedGun = Instantiate(gunToEquip, gunHolder.position, gunHolder.rotation) as Gun;
+        equippedGun.transform.parent = gunHolder;
+    }
+}
